b7b5f0ce52ced97a1a03fd57924eabb71397d961
adityahimaone <adityahimaone@gmail.com>
2024-01-19 15:43:57 +0700

    feat: add curve animation to layout

--- components/Layouts/curve/index.tsx
+++ components/Layouts/curve/index.tsx
@@ -1,28 +1,101 @@
+// https://blog.olivierlarose.com/articles/nextjs-page-transition-guide
 "use client";
 
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./index.module.scss";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { text, curve, translate } from "./anim";
+import { usePathname } from "next/navigation";
+
+interface IRoute {
+  [key: string]: string;
+}
+
+const routes: IRoute = {
+  "/transition": "Home",
+  "/transition/about": "About",
+  "/transition/contact": "Contact",
+};
 
 type Props = {
   children: React.ReactNode;
 };
 
-const Curve = ({ children }: Props) => {
-  const anim = (variants: any) => {
-    return {
-      initial: "initial",
-      animate: "enter",
-      exit: "exit",
-      variants,
-    };
+const anim = (variants: any) => {
+  return {
+    initial: "initial",
+    animate: "enter",
+    exit: "exit",
+    variants,
   };
+};
 
-  const notUsed = true;
+const Curve = ({ children }: Props) => {
+  const router = usePathname();
+  const [dimensions, setDimensions] = useState({
+    height: 0,
+    width: 0,
+  });
 
-  return <div className="page">{children}</div>;
+  useEffect(() => {
+    function resize() {
+      setDimensions({
+        width: window.innerWidth,
+
+        height: window.innerHeight,
+      });
+    }
+
+    resize();
+
+    window.addEventListener("resize", resize);
+
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
+  }, []);
+
+  console.log(dimensions, "dimensions");
+
+  return (
+    <div className={`page ${styles.curve}`}>
+      <div
+        style={{ opacity: dimensions.width == null ? 1 : 0 }}
+        className={styles.background}
+      />
+      <motion.p className={styles.route} {...anim(text)}>
+        {routes[router]}
+      </motion.p>
+      {dimensions.width > 0 && <SVG {...dimensions} />}
+      {children}
+    </div>
+  );
+};
+
+const SVG = ({ width, height }: { width: number; height: number }) => {
+  const initialPath = `
+    M0 300 
+    Q${width / 2} 0 ${width} 300
+    L${width} ${height + 300}
+    Q${width / 2} ${height + 600} 0 ${height + 300}
+    L0 0
+`;
+
+  const targetPath = `
+    M0 300
+    Q${width / 2} 0 ${width} 300
+    L${width} ${height}
+    Q${width / 2} ${height} 0 ${height}
+    L0 0
+`;
+
+  return (
+    <motion.svg {...anim(translate)}>
+      <motion.path {...anim(curve(initialPath, targetPath))} />
+    </motion.svg>
+  );
 };
 
 export default Curve;
